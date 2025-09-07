f6c5d19fa224480e3f408e3fb31d4c4a278debfe
Gibrail Zaidi <gibrailzaidi@gmail.com>
2025-09-07 15:46:01 +0530

    add

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-  employeeId: { type: String, ref: 'Employee' },
+  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
   date: { type: Date, default: Date.now },
-  time: { type: String, default: () => new Date().toTimeString().split(' ')[0] },
-  status: { type: String, enum: ['On Service', 'Terminated'], default: 'On Service' },
+  time: {
+    punch_in: { 
+      type: String, 
+      default: () => new Date().toTimeString().split(' ')[0] 
+    },
+    punch_out: { 
+      type: String, 
+      default: null 
+    }
+  },
+  status: { type: String, enum: ['Present', 'Absent'], default: 'Present' },
 });
+
 
 module.exports = mongoose.model('Attendance', attendanceSchema);
