7ed952f70f77419cc0177e317d8dd289dc174c99
hrithikrana <hrithikranaji@gmail.com>
2025-09-07 18:06:06 +0530

    UPDATE || few fixes for camera and add employ

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -7,30 +7,22 @@ const faceUtil = require('../utils/faceRecognition');
 // Add new employee
 exports.addEmployee = async (req, res) => {
   try {
-        const { emp_id, name, department, email } = req.body;
-        let photo = req.file; // uploaded file from multer
-
+    const { emp_id, name, department, email,faceEncoding } = req.body;
+    
     const existing = await Employee.findOne({ emp_id });
-        if (existing) return res.status(400).json({ message: "Employee ID already exists" });
+    if (existing) return res.status(400).json({ message: "Employee ID already exists" });
 
-        // Save photo
-        const photoPath = path.join(__dirname, '../uploads/', id + '.jpg');
-        fs.writeFileSync(photoPath, photo.buffer);
 
-        // Get face encoding
-       const encoding = await faceUtil.encodeImage(photoPath);
-
-       const employee = new Employee({ emp_id, name, department, email, faceEncoding: encoding });
-        // const employee = new Employee({ emp_id, name, department, email,});
+    const employee = new Employee({ emp_id, name, department, email, faceEncoding: faceEncoding });
     await employee.save();
 
     const msg = `Hi ${name},\nWelcome to the organization. You have been successfully registered.`;
-       await sendMail(email, 'Employee Registered', msg);
+    await sendMail.sendMail(email, 'Employee Registered', msg);
 
-    res.json({ message: "Employee added successfully" });
+    res.status(200).json({ message: "Employee added successfully" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Server Error" });
+    res.status(400).json({ message: "Server Error" });
   }
 };
 
